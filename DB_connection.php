d3d8ba2ed104b7e6e439abf7e633d1d5ef846085
CodingZone <dothedung131@gmail.com>
2023-09-21 21:51:01 +0700

    them admin/data/teacher

--- DB_connection.php
+++ DB_connection.php
@@ -1,14 +1,15 @@
-<?php
+<?php  
+
 $sName = "localhost";
 $uName = "root";
-$pass = "";
-$db_name = "sms_db";
-try {
-    $conn = new PDO("mysql:host=$sName;dbName=$db_name", $uName, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "connect failed" . $e->getMessage();
-    exit;
+$pass  = "";
+$db_name = "test";
 
-    //throw $th;
+try {
+	$conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $pass);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+	echo "Connection failed: ". $e->getMessage();
+	exit;
 }
+?>
