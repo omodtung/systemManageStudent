aed65ff8411c3edb27b63405379db8d468899ffa
CodingZone <dothedung131@gmail.com>
2023-09-23 10:41:38 +0700

    teacher in admin,get all teacher,and find unique name

---
+++ admin/data/teacherAd.php
@@ -0,0 +1,30 @@
+
+<?php  
+
+// tim kiem tat ca giao vien
+function getAllTeachers($conn){
+   $sql = "SELECT * FROM teachers";
+   $stmt = $conn->prepare($sql);
+   $stmt->execute();
+
+   if ($stmt->rowCount() >= 1) {
+     $teachers = $stmt->fetchAll();
+     return $teachers;
+   }else {
+   	return 0;
+   }
+}
+
+// kiem tra neu nhu ten nay doc nhat 
+function findNameDocNhat($uname, $conn){
+   $sql = "SELECT username FROM teachers
+           WHERE username=?";
+   $stmt = $conn->prepare($sql);
+   $stmt->execute([$uname]);
+
+   if ($stmt->rowCount() >= 1) {
+     return 0;
+   }else {
+   	return 1;
+   }
+}
