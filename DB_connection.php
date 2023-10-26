1c6cd975108e5de36828b2ced3e8399134423bd8
omodtung <dothedung131@gmail.com>
2023-10-26 17:23:59 +0700

    fast find with hoten using Ajax PHP

--- DB_connection.php
+++ DB_connection.php
@@ -6,7 +6,9 @@ $pass  = "";
 $db_name = "test7";
 
 try {
+	
 	$conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $pass);
+	
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e){
 	echo "Connection failed: ". $e->getMessage();
---
+++ admin/livesearch.php
@@ -0,0 +1,69 @@
+<?php
+
+// Close any existing database connections
+$pdo = null;
+
+// Try to connect to the database
+try {
+    $pdo = new PDO("mysql:host=localhost;dbname=test7", "root", "");
+} catch (PDOException $e) {
+    echo "Error connecting to the database: " . $e->getMessage();
+    exit;
+}
+
+// Try to execute the query
+try {
+    $input = $_POST['input'];
+    $query = "SELECT * FROM teachers WHERE hoten LIKE '{$input}%'";
+    $rows = $pdo->query($query)->fetchAll(PDO::FETCH_ASSOC);
+
+    // If there are any results, display them in a table
+    if (count($rows) > 0) {
+        echo '<table class="table table-striped">';
+        echo '<thead>';
+        echo '<tr>';
+        echo '<th scope="col">Teacher ID</th>';
+        echo '<th scope="col">User Name</th>';
+        echo '<th scope="col">Ho Ten</th>';
+        echo '<th scope="col">Ma Mon Hoc</th>';
+        echo '<th scope="col">Ngay Sinh</th>';
+        echo '<th scope="col">Gioi Tinh</th>';
+        echo '<th scope="col">Dia Chi</th>';
+        echo '<th scope="col">Ma Giao Vien</th>';
+        echo '<th scope="col">Ma Khoi</th>';
+        echo '<th scope="col">Action</th>';
+        echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+
+        foreach ($rows as $row) {
+            echo '<tr>';
+            echo '<td>' . $row['id'] . '</td>';
+            echo '<td>' . $row['username'] . '</td>';
+            echo '<td>' . $row['hoten'] . '</td>';
+            echo '<td>' . $row['mamonhoc'] . '</td>';
+            echo '<td>' . $row['ngaysinh'] . '</td>';
+            echo '<td>' . $row['gioitinh'] . '</td>';
+            echo '<td>' . $row['diachi'] . '</td>';
+            echo '<td>' . $row['magv'] . '</td>';
+            echo '<td>' . $row['makhoi'] . '</td>';
+            echo '<td>';
+            echo '<a href="teacher-edit.php?idteach=' . $row['id'] . '" class="btn btn-warning">Edit</a>';
+            echo '<a href="" class="btn btn-danger">Delete</a>';
+            echo '</td>';
+            echo '</tr>';
+        }
+
+        echo '</tbody>';
+        echo '</table>';
+    } else {
+        echo '<h3>No teachers found.</h3>';
+    }
+} catch (PDOException $e) {
+    echo "Error executing the query: " . $e->getMessage();
+}
+
+// Close the database connection
+$pdo = null;
+
+?>
