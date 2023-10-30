e29b95a3110c1575a210488aa645dc4c5aa40585
lehoang <tranbaolong00zz@gmail.com>
2023-10-30 18:08:46 +0700

    fixed export student

--- admin/req/exportteacher.php
+++ admin/req/exportteacher.php
@@ -12,13 +12,14 @@ require_once '../inc/PhpXlsxGenerator.php';
 $fileName = "teachers-data_" . date('Y-m-d') . ".xlsx"; 
  
 // Define column names 
-$excelData[] = array('ID', 'magv', 'username', 'hoten','mamonhoc','makhoi','ngaysinh', 'gioitinh', 'diachi', 'STATUS'); 
  
 if(isset($_GET["searched"]) && $_GET["searched"] != ""){
+    $excelData[] = array('ID', 'magv', 'username', 'hoten','mamonhoc','makhoi','ngaysinh', 'gioitinh', 'diachi', 'STATUS'); 
     $sql = "SELECT * FROM teachers WHERE hoten LIKE '". $_GET['searched'] ."%' ORDER BY id ASC";
 }
 else if(isset($_GET["studentsearched"]) && $_GET["studentsearched"] != ""){
-    $sql = "SELECT * FROM students WHERE hotenhs LIKE '". $_GET['searched'] ."%' ORDER BY id ASC";
+    $excelData[] = array('id', 'username', 'mahs', 'makhoi','malop','hotenhs','hanhkiem','HocLuc','ngaysinh', 'gioitinh', 'diachi', 'STATUS'); 
+    $sql = "SELECT * FROM students JOIN avgscore ON students.id = avgscore.student_id WHERE hotenhs LIKE '". $_GET['studentsearched'] ."%' ORDER BY id ASC";
 }
 else{
     $sql = "SELECT * FROM teachers ORDER BY id ASC"; 
@@ -30,7 +31,8 @@ $stmt->execute();
 if($stmt->rowCount() > 0){ 
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)){ 
         $status = ($row['status'] == 1)?'Active':'Inactive'; 
-        $lineData = array($row['id'], $row['magv'], $row['username'], $row['hoten'],$row['mamonhoc'],$row['makhoi'], $row['ngaysinh'], $row['gioitinh'], $row['diachi'], $status);  
+        if(isset($_GET["studentsearched"])) $lineData = array($row['id'], $row['username'], $row['mahs'], $row['makhoi'],$row['malop'],$row['hotenhs'],$row['hanhkiem'],$row['HocLuc'], $row['ngaysinh'], $row['gioitinh'], $row['diachi'], $status);  
+        else $lineData = array($row['id'], $row['magv'], $row['username'], $row['hoten'],$row['mamonhoc'],$row['makhoi'], $row['ngaysinh'], $row['gioitinh'], $row['diachi'], $status);  
         $excelData[] = $lineData; 
     } 
 } 
