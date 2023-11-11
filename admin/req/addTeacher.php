9b0ae74e770440b0a8344620d5add34b51c854d4
omodtung <dothedung131@gmail.com>
2023-11-11 10:32:20 +0700

    commit avoid conflict

--- admin/req/addTeacher.php
+++ admin/req/addTeacher.php
@@ -14,6 +14,8 @@ if (
     if ($_SESSION['role'] == 'Admin') {
 
         if (
+
+            
             isset($_POST['flname']) &&
             isset($_POST['idGV']) &&
             isset($_POST['uname']) &&
