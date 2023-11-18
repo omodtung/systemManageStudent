176208fb8f2574a1020572369b192366adf2a1f1
lehoang <tranbaolong00zz@gmail.com>
2023-11-18 16:45:50 +0700

    delete

--- admin/teacherNewUI.php
+++ admin/teacherNewUI.php
@@ -246,7 +246,7 @@ if (isset($_SESSION['admin_id']) && isset($_SESSION['role'])) {
                                                     Edit
                                                 </button>
                                                 <button type="button" data-bs-toggle="modal" data-bs-target="#modalinfo" onclick="btnclickinfo('./inc/TeacherInfo.php?idteach=<?= $teacher['id'] ?>')" data-bs-id=<?= $teacher['id'] ?> class="btn btn-info">Info</button>
-                                                <a href="" class="btn btn-danger">Delete</a>
+                                                <a href="./applogic/deleteteacher.php" class="btn btn-danger">Delete</a>
 
 
 
